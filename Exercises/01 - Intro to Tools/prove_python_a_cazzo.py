45ebab6eb25a9e3e18078f99d3c01e96c9af03ef
Alessandro Amella <44754837+alessandroamella@users.noreply.github.com>
2026-09-04 10:27:46 +0200

    boh in teoria il primo fatto

---
+++ Exercises/01 - Intro to Tools/prove_python_a_cazzo.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# test_range = np.arange(10)
+# print("Test range:", test_range)
+
+# linspaced = np.linspace(0, 1, 5)
+# print("Linspaced:", linspaced)
+
+# id_matrix = np.eye(3)
+# print("Identity matrix:", id_matrix)
